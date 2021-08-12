9e7f9a6056ffd26851c91dc1a83eadd3d1a4b2ac
Mason Chen <jiec@microsoft.com>
2021-08-12 16:53:01 +0800

    add stubs for create/set/show/delete parameters

---
+++ src/spring-cloud/azext_spring_cloud/buildpacks_binding.py
@@ -0,0 +1,11 @@
+def buildpacks_binding_set(cmd, client, service, resource_group):
+    pass
+
+def buildpacks_binding_create(cmd, client, service, resource_group):
+    pass
+
+def buildpacks_binding_show(cmd, client, service, resource_group):
+    pass
+
+def buildpacks_binding_delete(cmd, client, service, resource_group):
+    pass
