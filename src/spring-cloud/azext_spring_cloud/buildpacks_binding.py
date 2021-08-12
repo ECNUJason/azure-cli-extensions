cb223f6546eda6b5012d1be12f3d4a5787ef0e7d
Mason Chen <jiec@microsoft.com>
2021-08-12 17:46:33 +0800

    add parameter to sub command stud

--- src/spring-cloud/azext_spring_cloud/buildpacks_binding.py
+++ src/spring-cloud/azext_spring_cloud/buildpacks_binding.py
@@ -1,11 +1,13 @@
-def buildpacks_binding_set(cmd, client, service, resource_group):
+def buildpacks_binding_create(cmd, client, service, resource_group,
+                              name, type, properties=None, secrets=None):
     pass
 
-def buildpacks_binding_create(cmd, client, service, resource_group):
+def buildpacks_binding_set(cmd, client, service, resource_group,
+                           name, type, properties=None, secrets=None):
     pass
 
-def buildpacks_binding_show(cmd, client, service, resource_group):
+def buildpacks_binding_show(cmd, client, service, resource_group, name):
     pass
 
-def buildpacks_binding_delete(cmd, client, service, resource_group):
+def buildpacks_binding_delete(cmd, client, service, resource_group, name):
     pass
